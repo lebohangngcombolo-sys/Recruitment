0f23b891783fc24f9fcfefd3485aa4127b5ad9e0
Dzunisani007 <dzunisanimabunda85@gmail.com>
2026-02-12 10:12:17 +0200

    Prep deployment: Flutter fixes + restore OCR services

--- khono_recruite/lib/screens/candidate/job_details_page.dart
+++ khono_recruite/lib/screens/candidate/job_details_page.dart
@@ -1238,7 +1238,7 @@ class _JobDetailsPageState extends State<JobDetailsPage> {
               _socialIcon('assets/icons/Instagram1.png',
                   'https://www.instagram.com/yourprofile'),
               _socialIcon('assets/icons/x1.png', 'https://x.com/yourprofile'),
-              _socialIcon('assets/icons/LinkedIn1.png',
+              _socialIcon('assets/icons/LinkedIn.png',
                   'https://www.linkedin.com/in/yourprofile'),
               _socialIcon('assets/icons/facebook1.png',
                   'https://www.facebook.com/yourprofile'),
